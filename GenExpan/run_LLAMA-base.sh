fb7dfd8d2dcbbb938f29dc76384c7b0348dd3d11
lqs <lv.qingsong@foxmail.com>
2024-03-02 06:55:19 +0800

    Delete GenExpan directory

--- GenExpan/run_LLAMA-base.sh
+++
@@ -1,9 +0,0 @@
-
-
-
-export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-
-
-python -u src/main.py \
-        --expand_results="expand_results_LLAMA-base" \
-        --model_path="train_output/checkpoint-132" \
